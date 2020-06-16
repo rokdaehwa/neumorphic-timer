ba3cc0fd8ac0fea200533ead0e278264b10bb9be
rokdaehwa <kartlagga@gmail.com>
2020-06-17 00:42:56 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { CircularProgress, Button } from "@material-ui/core";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
+
+const timerSize = 500;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+    height: "100%",
+  },
+  progressWrapper: {
+    margin: "0 auto",
+    width: timerSize,
+    height: timerSize,
+    borderRadius: timerSize,
+    boxShadow: "20px 20px 60px #c4c4c4, -20px -20px 60px #ffffff",
+    padding: 10,
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+  const [progress, setProgress] = React.useState(0);
+  const [total, setTotal] = React.useState(60);
+  const [isPlaying, setIsPlaying] = React.useState(false);
+
+  const handleProgress = (progress) => {
+    setProgress(progress);
+  };
+
+  const tick = () => {
+    if (progress >= total * 60) return;
+    setProgress(progress + 1);
+    console.log("progress: " + progress);
+  };
+
+  const handleTogglePlaying = () => {
+    setIsPlaying(!isPlaying);
+  };
+
+  React.useEffect(() => {
+    console.log("useEffect");
+    const interval = setInterval(() => {
+      if (isPlaying) {
+        tick();
+      } else {
+        clearInterval(interval);
+      }
+    }, 1000);
+    return () => clearInterval(interval);
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <div className={classes.progressWrapper}>
+        <CircularProgress
+          size={timerSize}
+          thickness={22}
+          value={100 * (1 - progress/(60 * total))}
+          variant="static"
+        />
+      </div>
+      <Button onClick={handleTogglePlaying}>Tick</Button>
+      <div>{progress}</div>
     </div>
   );
 }
